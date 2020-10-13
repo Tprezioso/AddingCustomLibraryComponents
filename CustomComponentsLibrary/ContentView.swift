2fe1069dfd98e263d0215f4ccda6b598282ce4fb
Tprezioso <tommyprezioso@gmail.com>
2020-10-13 15:18:24 -0400

    Initial Commit

---
+++ CustomComponentsLibrary/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomComponentsLibrary
+//
+//  Created by Thomas Prezioso on 10/13/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomComponentsLibrary/CustomComponentsLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomComponentsLibraryApp.swift
+//  CustomComponentsLibrary
+//
+//  Created by Thomas Prezioso on 10/13/20.
+//
+
+import SwiftUI
+
+@main
+struct CustomComponentsLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
